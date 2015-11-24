326c485e3f808365f7fb8e7ab56ddd99bb349da3
Christopher Brown <io@henrian.com>
2015-11-23 23:42:57 -0600

    Remove type_declarations/

--- type_declarations/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="DefinitelyTyped/angularjs/angular.d.ts" />
-/// <reference path="DefinitelyTyped/lodash/lodash.d.ts" />
-
-/// <reference path="../node_modules/httprequest/httprequest.d.ts" />
-/// <reference path="../node_modules/notify-ui/notify-ui.d.ts" />
-
-declare var MediaStreamTrack;
-declare var require;
